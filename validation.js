0a8a6c77a1329c4a7b6ea931da3d10a5deaac7cf
Skafeer <skafeerr@gmail.com>
2025-06-17 22:54:00 +0300

    Delete validation.js

--- validation.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('form').addEventListener('submit', function (event) {
-    const price = document.querySelector('#price').value;
-    if (price <= 0) {
-        alert('يرجى إدخال سعر صحيح.');
-        event.preventDefault();
-    }
-});
