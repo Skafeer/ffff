34895714bbe38236da320e1a97343075b7848265
Skafeer <skafeerr@gmail.com>
2025-06-17 22:53:25 +0300

    Delete header.php

--- header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <h1>الأنيس للإلكترونيات</h1>
-    <nav>
-        <a href="index.html">الرئيسية</a> | 
-        <a href="dashboard.html">لوحة التاجر</a> | 
-        <a href="admin.html">لوحة الإدارة</a>
-    </nav>
-</header>
